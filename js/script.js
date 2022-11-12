9ef0d1e66dd3373641c02130667406a38a081f1d
Chi Cuong Tang <chicuong.tang01@gmail.com>
2022-11-12 15:01:44 +0700

    merge all pages into 1 page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
     showSlides(slideIndex);
+
+    const modal = document.getElementById("slideshow");
+    const closeModalBtn = document.getElementsByClassName("close")[0];
+    let modalToggle = document.getElementById("slideshow-toggle");
+    modalToggle.onclick = function () {
+        modal.style.display = "block";
+    };
+    closeModalBtn.onclick = function () {
+        modal.style.display = "none";
+    };
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
 });
 
 let slideIndex = 1;
@@ -23,4 +39,10 @@ function showSlides(n) {
         slides[i].style.display = "none";
     }
     slides[slideIndex - 1].style.display = "block";
+}
+
+function scrollToSection(id) {
+    window.event.preventDefault();
+    let el = document.getElementById(id);
+    el.scrollIntoView();
 }
